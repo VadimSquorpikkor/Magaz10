831fd1ab994bda8ed63587ed3f7678554e5b9cd3
VadimSquorpikkor <VadimSerikov11@gmail.com>
2017-03-12 11:26:42 +0300

    1

---
+++ app/src/main/java/com/squorpikkor/app/magaz10/MainActivity.java
@@ -0,0 +1,38 @@
+package com.squorpikkor.app.magaz10;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.SetValueButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.SetValueButton:
+                        clickActivity(Main2Activity.class);
+                        break;
+
+                }
+            }
+        };
+
+        button1.setOnClickListener(listener);
+    }
+
+    private void clickActivity(Class c) {
+        startActivity(new Intent(this, c));
+    }
+}
